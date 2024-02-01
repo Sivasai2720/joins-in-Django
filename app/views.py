91fb48f17ab8da1dfa147ccb40bdfde4351a3122
Sivasai2720 <sivasaikumarkongala@gmail.com>
2024-02-01 15:17:44 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -29,3 +29,27 @@ def equijoins(request):
 
 
 
+def selfjoin(request):
+    EMobjects=Emp.objects.select_related('mgr').all()
+    EMobjects=Emp.objects.select_related('mgr').filter(hiredate__year=1981)
+    EMobjects=Emp.objects.select_related('mgr').filter(sal__gt=1250)
+    EMobjects=Emp.objects.select_related('mgr').filter(comm__isnull=True)
+    EMobjects=Emp.objects.select_related('mgr').filter(comm__isnull=True,sal__lt=1250)
+    EMobjects=Emp.objects.select_related('mgr').all()
+
+    #------For Writing Conditions-------
+    EMobjects=Emp.objects.select_related('mgr').filter(ename='SMITH')   #syn--> Col name= value,   which do u want, when u writing condtion for Child table
+    EMobjects=Emp.objects.select_related('mgr').filter(sal__lte=1500)  
+
+
+
+    #--------For Printing the Data----------
+    EMobjects=Emp.objects.select_related('mgr').filter(deptno=10)    #syn--> obj name.colname,   which do u want, when u printing the data for Child table
+    #syn--> Obj name.deptno.Col name, which do u want, when u printing the data for parent table
+
+    EMobjects=Emp.objects.select_related('mgr').all()
+
+    
+
+    d={'EMobjects':EMobjects}
+    return render(request,'selfjoin.html',d)
